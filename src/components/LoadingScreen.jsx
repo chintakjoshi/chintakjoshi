a78f04752c1a38627bab3db41d2db409012450d4
chintakjoshi <chintakmanojkumar.joshi@slu.edu>
2024-03-27 13:29:42 -0500

    modifying loader

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import './loading.css'; // Ensure you create a corresponding CSS file
 
 const LoadingScreen = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div>
-        <img
-          src="https://cdn.dribbble.com/users/450343/screenshots/2465762/media/97b14be7af51dcea4d8ae244c545769b.gif"
-          alt=""
-        />
+      <div className="loader">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
       </div>
     </div>
   );
