32f26508d63e221f19b61dad5bfa5049e1ebc21b
Chintak Joshi <113061278+chintakjoshi@users.noreply.github.com>
2024-05-01 17:21:02 -0500

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
             </div>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
-                <a href="https://drive.google.com/file/d/1AU68BwbIVqTcAk1EY65x0hhNEcKzHnmm/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
+                <a href="https://drive.google.com/file/d/1Rv6nO5ngVDiy88kj2x30FvD3ZCRT-lPk/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
                 Resume
                 </a>
               </div>
