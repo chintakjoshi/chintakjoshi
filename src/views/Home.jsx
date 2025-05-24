bacfcb1d08a3757e5f76691640bdccef7e4eff53
chintakjoshi <chintakjoshi36@gmail.com>
2025-05-23 20:10:00 -0400

    making modifications

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -51,8 +51,7 @@ const Home = () => {
                   : "mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
               }
             >
-              I am a Full-Stack Software Engineer. I am currently working at
-              Saint Louis University.
+              I am currently working at Singlewire Software as a Software Engineer III.
             </p>
             <div className="flex md:justify-start ">
               {contactLinks.map((el) => (
@@ -68,7 +67,7 @@ const Home = () => {
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
                 <a href="https://drive.google.com/file/d/1Rv6nO5ngVDiy88kj2x30FvD3ZCRT-lPk/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-                Resume
+                  Resume
                 </a>
               </div>
             </div>
